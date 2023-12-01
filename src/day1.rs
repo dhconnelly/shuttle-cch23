718ec21d5533734e537f9a2ecc8847045dfce36a
Daniel Connelly <dhconnelly@gmail.com>
2023-12-01 19:22:42 +0100

    day1: small cleanups

--- src/day1.rs
+++ src/day1.rs
@@ -1,13 +1,13 @@
 use crate::extractors::PathVec;
 use axum::{http::StatusCode, routing::get, Router};
 
-async fn part1(PathVec(nums): PathVec<i64>) -> Result<String, StatusCode> {
+async fn solve(PathVec(nums): PathVec<i64>) -> Result<String, StatusCode> {
     let answer = nums.into_iter().reduce(|x, y| x ^ y).unwrap_or(0).pow(3);
     Ok(answer.to_string())
 }
 
 pub fn router() -> Router {
-    Router::new().route("/*nums", get(part1))
+    Router::new().route("/*nums", get(solve))
 }
 
 #[cfg(test)]
@@ -18,14 +18,11 @@ mod test {
     use tower::ServiceExt;
 
     async fn request(uri: &str) -> i64 {
-        let app = router();
         let req = Request::builder().uri(uri).body(Body::empty()).unwrap();
-        let res = app.oneshot(req).await.unwrap();
+        let res = router().oneshot(req).await.unwrap();
         assert_eq!(res.status(), StatusCode::OK);
         let data = res.into_body().collect().await.unwrap().to_bytes();
-        let answer = String::from_utf8(data.to_vec()).unwrap();
-        println!("{}", answer);
-        i64::from_str_radix(&answer, 10).unwrap()
+        String::from_utf8(data.to_vec()).unwrap().parse().unwrap()
     }
 
     #[tokio::test]
--- src/extractors.rs
+++ src/extractors.rs
@@ -11,19 +11,16 @@ pub struct PathVec<T: FromStr>(pub Vec<T>);
 #[async_trait]
 impl<S: Send + Sync, T: FromStr> FromRequestParts<S> for PathVec<T> {
     type Rejection = StatusCode;
-
     async fn from_request_parts(
         parts: &mut Parts,
         _state: &S,
     ) -> Result<Self, Self::Rejection> {
         let path = parts.uri.path();
-        let components = path
+        let components = path[1..]
             .split('/')
-            .skip(1)
             .map(|s| s.parse())
             .collect::<Result<Vec<T>, _>>()
             .map_err(|_| StatusCode::BAD_REQUEST)?;
-        let result = Self(components);
-        Ok(result)
+        Ok(Self(components))
     }
 }
